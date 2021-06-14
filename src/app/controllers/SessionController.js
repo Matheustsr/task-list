bf208e8dd352e39c1979501c476309590a639225
Matheus Vieira <matheustsr@outlook.com>
2021-06-14 08:47:23 -0300

    JWT auth config.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,33 @@
 import jwt from 'jsonwebtoken';
-import * as Yup from 'yup';
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
-        return res.json({ ok: true });
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Usuário não existe!' });
+        }
+
+        if (!(await user.checkPassword(password))) {
+            return res.status(401).json({ error: 'Senha incorreta!' });
+        }
+
+        const { id, name } = user;
+
+        return res.json({
+            user: {
+                id,
+                name,
+                email,
+            },
+            token: jwt.sign({ id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+            }),
+        });
     }
 }
 export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
