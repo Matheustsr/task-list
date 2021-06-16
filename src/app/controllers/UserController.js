eb99f6a379c8f961b102dbce5cdd940f57ba281f
unknown <matheustsr@outlook.com>
2021-06-16 06:53:15 -0300

    validacao de campos para update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -31,6 +31,28 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            // for validation
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string()
+                .min(6)
+                .when(
+                    'oldPassword',
+                    (oldPassword, field) =>
+                        oldPassword ? field.required() : field // informa que obrigatorio
+                ),
+
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha ao validar!' });
+        }
+
         const { email, oldPassword } = req.body;
 
         const user = await User.findByPk(req.userId); // busca pelo user baseado no ID (pk)
