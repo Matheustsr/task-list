bc53d6535687888a461271b843c93db92a970019
Matheus vieira <matheustsr@outlook.com>
2021-07-06 04:53:02 -0300

    hash encrypt fix

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -17,6 +17,7 @@ class User extends Model {
         this.addHook('beforeSave', async (user) => {
             if (user.password) {
                 user.password_hash = await bcrypt.hash(user.password, 8);
+                user.password_hash = await bcrypt.hash(user.password, 8);
             }
         });
 
