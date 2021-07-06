268d413c1fa9f56b4e8d76892dda20f0b5453005
Matheus vieira <matheustsr@outlook.com>
2021-07-06 04:52:16 -0300

    update model user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -8,6 +8,7 @@ class User extends Model {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
                 password: Sequelize.VIRTUAL,
+                password: Sequelize.VIRTUAL,
                 password_hash: Sequelize.STRING,
             },
             {
